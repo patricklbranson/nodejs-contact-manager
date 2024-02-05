6449abdc42432948d2fa8f3299677b598d34e381
patrick-branson <plbranson@patrickbranson.com>
2024-02-04 19:59:46 -0500

    Updated validate.middleware.js

--- middleware/validate.middleware.js
+++ middleware/validate.middleware.js
@@ -13,3 +13,29 @@
  *  See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const validateToken = asyncHandler(async (req, res) => {
+  let token;
+  let authHeader = req.headers.Authorization || req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        throw new Error("User is Not Authorized");
+      }
+      req.user = decoded.user;
+      next();
+    });
+
+    if (!token) {
+      res.status(401);
+      throw new Error("User is Not Authorized or Token is Missing!");
+    }
+  }
+});
+
+module.exports = { validateToken };
